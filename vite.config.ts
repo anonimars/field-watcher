48211e46701c4e5bb3e2f9da7afea78a2789031f
Kriss Mwihu <krissmwihu@gmail.com>
2026-04-24 13:02:19 +0300

    replace lovable vite config with standard config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tsconfigPaths(),
+  ],
   build: {
     outDir: "dist/client",
   },
